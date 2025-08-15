5562e37471a520b05cf0fb525f6578bcb04c594b
Dan Nicholls <dannicholls12@gmail.com>
2025-08-15 18:09:46 +1000

    feat: enable Server struct to make outbound requests
    - Added the `apiGet` method to the Server struct
    - Updated handleHome to fetch API info

--- frontend/internal/server/handlers.go
+++ frontend/internal/server/handlers.go
@@ -2,18 +2,36 @@ package server
 
 import (
 	"net/http"
+	"encoding/json"
+	"context"
+	"fmt"
 
 	"github.com/dan-nicholls/danlovesto.run/frontend/internal/ui/pages"
 )
 
 type Info struct {
-	Uptime string
-	Version string
+	Uptime string `json:"uptime"`
+	Version string `json:"version"`
 }
 
 func (s *Server) handleHome(w http.ResponseWriter, r *http.Request) {
-	// TODO - Parse info properly
 	ctx := r.Context()
-	info := Info{ Uptime: "1hr30min", Version: "v1.0.0" }
+	var info Info
+	_ = s.apiGet(ctx, "/info", &info)
 	pages.Home(pages.Info{Version: info.Version, Uptime: info.Uptime}).Render(ctx, w)
+}
+
+func (s *Server) apiGet(ctx context.Context, path string, v any) error {
+	url := fmt.Sprintf("%s%s", s.apiURL, path)
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode >= 300 {
+		return fmt.Errorf("api %s -> %d", path, res.StatusCode)
+	}
+	return json.NewDecoder(res.Body).Decode(v)
 }
